a90693b74266ee889d8e6e213c353143ea1715b4
Linus Ramneborg <uberhaxor101@live.com>
2017-09-11 11:56:02 +0200

    Might as well :shipit:

---
+++ main.lua
@@ -0,0 +1,19 @@
+local serializer = require 'apis.serializer'
+local ui = require 'apis.UI'
+local button = require 'apis.buttonAPI'
+local window = require 'apis.windowAPI'
+-- Multitask using APIs
+
+function love.load()
+
+end
+
+
+function love.update()
+
+end
+
+
+function love.graphics()
+
+end
